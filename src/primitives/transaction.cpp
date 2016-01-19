zcash/zcash

Added CPourTx verification and construction routines.

src/primitives/transaction.cpp
@@ -9,6 +9,87 @@
 #include "tinyformat.h"
 #include "utilstrencodings.h"
 
+#include "libzerocash/PourProver.h"
+#include "libzerocash/PourTransaction.h"
+
+template<std::size_t N>
+boost::array<std::vector<unsigned char>, N> uint256_to_array(const boost::array<uint256, N>& in) {
+    boost::array<std::vector<unsigned char>, N> result;
+    for (int i = 0; i < N; i++) {
+        result[i] = std::vector<unsigned char>(in[i].begin(), in[i].end());
+    }
+
+    return result;
+}
+
+template<std::size_t N>
+boost::array<uint256, N> unsigned_char_to_uint256_array(const boost::array<std::vector<unsigned char>, N>& in) {
+    boost::array<uint256, N> result;
+    for (int i = 0; i < N; i++) {
+        result[i] = uint256(in[i]);
+    }
+
+    return result;
+}
+
+CPourTx::CPourTx(ZerocashParams& params,
+            const CScript& scriptPubKey,
+            const uint256& anchor,
+            const boost::array<PourInput, NUM_POUR_INPUTS>& inputs,
+            const boost::array<PourOutput, NUM_POUR_OUTPUTS>& outputs,
+            CAmount vpub_old,
+            CAmount vpub_new) : scriptSig(), scriptPubKey(scriptPubKey), vpub_old(vpub_old), vpub_new(vpub_new), anchor(anchor)
+{
+    uint256 scriptPubKeyHash;
+    {
+        CHashWriter ss(SER_GETHASH, 0);
+        ss << scriptPubKey;
+        scriptPubKeyHash = ss.GetHash();
+    }
+
+    PourTransaction pourtx(params,
+                           std::vector<unsigned char>(scriptPubKeyHash.begin(), scriptPubKeyHash.end()),
+                           std::vector<unsigned char>(anchor.begin(), anchor.end()),
+                           std::vector<PourInput>(inputs.begin(), inputs.end()),
+                           std::vector<PourOutput>(outputs.begin(), outputs.end()),
+                           vpub_old,
+                           vpub_new);
+
+    boost::array<std::vector<unsigned char>, NUM_POUR_INPUTS> serials_bv;
+    boost::array<std::vector<unsigned char>, NUM_POUR_OUTPUTS> commitments_bv;
+    boost::array<std::vector<unsigned char>, NUM_POUR_INPUTS> macs_bv;
+    boost::array<std::string, NUM_POUR_OUTPUTS> ciphertexts_bv;
+
+    proof = pourtx.unpack(serials_bv, commitments_bv, macs_bv, ciphertexts_bv);
+    serials = unsigned_char_to_uint256_array(serials_bv);
+    commitments = unsigned_char_to_uint256_array(commitments_bv);
+    macs = unsigned_char_to_uint256_array(macs_bv);
+
+    ciphertexts = ciphertexts_bv;
+}
+
+bool CPourTx::Verify(ZerocashParams& params) const {
+    // Compute the hash of the scriptPubKey.
+    uint256 scriptPubKeyHash;
+    {
+        CHashWriter ss(SER_GETHASH, 0);
+        ss << scriptPubKey;
+        scriptPubKeyHash = ss.GetHash();
+    }
+
+    return PourProver::VerifyProof(
+        params,
+        std::vector<unsigned char>(scriptPubKeyHash.begin(), scriptPubKeyHash.end()),
+        std::vector<unsigned char>(anchor.begin(), anchor.end()),
+        vpub_old,
+        vpub_new,
+        uint256_to_array<NUM_POUR_INPUTS>(serials),
+        uint256_to_array<NUM_POUR_OUTPUTS>(commitments),
+        uint256_to_array<NUM_POUR_INPUTS>(macs),
+        proof
+    );
+}
+
 std::string COutPoint::ToString() const
 {
     return strprintf("COutPoint(%s, %u)", hash.ToString().substr(0,10), n);

zerocurrencycoin/Zero

src/primitives/transaction.cpp
@@ -9,6 +9,87 @@
 #include "tinyformat.h"
 #include "utilstrencodings.h"
 
+#include "libzerocash/PourProver.h"
+#include "libzerocash/PourTransaction.h"
+
+template<std::size_t N>
+boost::array<std::vector<unsigned char>, N> uint256_to_array(const boost::array<uint256, N>& in) {
+    boost::array<std::vector<unsigned char>, N> result;
+    for (size_t i = 0; i < N; i++) {
+        result[i] = std::vector<unsigned char>(in[i].begin(), in[i].end());
+    }
+
+    return result;
+}
+
+template<std::size_t N>
+boost::array<uint256, N> unsigned_char_vector_array_to_uint256_array(const boost::array<std::vector<unsigned char>, N>& in) {
+    boost::array<uint256, N> result;
+    for (size_t i = 0; i < N; i++) {
+        result[i] = uint256(in[i]);
+    }
+
+    return result;
+}
+
+CPourTx::CPourTx(ZerocashParams& params,
+            const CScript& scriptPubKey,
+            const uint256& anchor,
+            const boost::array<PourInput, NUM_POUR_INPUTS>& inputs,
+            const boost::array<PourOutput, NUM_POUR_OUTPUTS>& outputs,
+            CAmount vpub_old,
+            CAmount vpub_new) : scriptSig(), scriptPubKey(scriptPubKey), vpub_old(vpub_old), vpub_new(vpub_new), anchor(anchor)
+{
+    uint256 scriptPubKeyHash;
+    {
+        CHashWriter ss(SER_GETHASH, 0);
+        ss << scriptPubKey;
+        scriptPubKeyHash = ss.GetHash();
+    }
+
+    PourTransaction pourtx(params,
+                           std::vector<unsigned char>(scriptPubKeyHash.begin(), scriptPubKeyHash.end()),
+                           std::vector<unsigned char>(anchor.begin(), anchor.end()),
+                           std::vector<PourInput>(inputs.begin(), inputs.end()),
+                           std::vector<PourOutput>(outputs.begin(), outputs.end()),
+                           vpub_old,
+                           vpub_new);
+
+    boost::array<std::vector<unsigned char>, NUM_POUR_INPUTS> serials_bv;
+    boost::array<std::vector<unsigned char>, NUM_POUR_OUTPUTS> commitments_bv;
+    boost::array<std::vector<unsigned char>, NUM_POUR_INPUTS> macs_bv;
+    boost::array<std::string, NUM_POUR_OUTPUTS> ciphertexts_bv;
+
+    proof = pourtx.unpack(serials_bv, commitments_bv, macs_bv, ciphertexts_bv);
+    serials = unsigned_char_vector_array_to_uint256_array(serials_bv);
+    commitments = unsigned_char_vector_array_to_uint256_array(commitments_bv);
+    macs = unsigned_char_vector_array_to_uint256_array(macs_bv);
+
+    ciphertexts = ciphertexts_bv;
+}
+
+bool CPourTx::Verify(ZerocashParams& params) const {
+    // Compute the hash of the scriptPubKey.
+    uint256 scriptPubKeyHash;
+    {
+        CHashWriter ss(SER_GETHASH, 0);
+        ss << scriptPubKey;
+        scriptPubKeyHash = ss.GetHash();
+    }
+
+    return PourProver::VerifyProof(
+        params,
+        std::vector<unsigned char>(scriptPubKeyHash.begin(), scriptPubKeyHash.end()),
+        std::vector<unsigned char>(anchor.begin(), anchor.end()),
+        vpub_old,
+        vpub_new,
+        uint256_to_array<NUM_POUR_INPUTS>(serials),
+        uint256_to_array<NUM_POUR_OUTPUTS>(commitments),
+        uint256_to_array<NUM_POUR_INPUTS>(macs),
+        proof
+    );
+}
+
 std::string COutPoint::ToString() const
 {
     return strprintf("COutPoint(%s, %u)", hash.ToString().substr(0,10), n);