Matthelonianxl/zcash

Add Sapling support to z_shieldcoinbase
Part of #3216.

qa/rpc-tests/wallet_shieldcoinbase_sapling.py
@@ -1,0 +1,13 @@
+#!/usr/bin/env python2
+import inspect
+import os
+
+cwd = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+execfile(os.path.join(cwd, 'wallet_shieldcoinbase.py'))
+
+class WalletShieldCoinbaseSprout(WalletShieldCoinbaseTest):
+    def __init__(self):
+        super(WalletShieldCoinbaseSprout, self).__init__('sapling')
+
+if __name__ == '__main__':
+    WalletShieldCoinbaseSprout().main()

zerocurrencycoin/Zero

qa/rpc-tests/wallet_shieldcoinbase_sapling.py
@@ -1,0 +1,13 @@
+#!/usr/bin/env python2
+import inspect
+import os
+
+cwd = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+execfile(os.path.join(cwd, 'wallet_shieldcoinbase.py'))
+
+class WalletShieldCoinbaseSapling(WalletShieldCoinbaseTest):
+    def __init__(self):
+        super(WalletShieldCoinbaseSapling, self).__init__('sapling')
+
+if __name__ == '__main__':
+    WalletShieldCoinbaseSapling().main()