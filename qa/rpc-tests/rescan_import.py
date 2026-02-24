2b681af75f5dc29ad220e44cb05207a872f016b8
tearodactyl <tearodactylus@gmail.com>
2026-02-24 15:46:34 -0800

    New tests

---
+++ qa/rpc-tests/rescan_import.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# Copyright (c) 2025 The Zcash developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
+#
+# P1: Test z_importkey with rescan=yes updates balance correctly
+#
+
+import sys; assert sys.version_info < (3,), ur"This script does not run under Python 3. Please use Python 2.7.x."
+
+from decimal import Decimal
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    get_coinbase_address,
+    start_nodes,
+    wait_and_assert_operationid_status,
+)
+
+
+class RescanImportTest(BitcoinTestFramework):
+
+    def setup_nodes(self):
+        return start_nodes(4, self.options.tmpdir, [[
+            '-experimentalfeatures', '-zmergetoaddress',
+        ]] * 4)
+
+    def run_test(self):
+        # Sanity-check the test harness
+        assert_equal(self.nodes[0].getblockcount(), 200)
+
+        addrs = [utxo['address'] for utxo in self.nodes[0].listunspent() if utxo.get('generated')]
+        if not addrs:
+            print("Skipping rescan_import: get_coinbase_address fails (no generated utxos)")
+            return
+        taddr = get_coinbase_address(self.nodes[0])
+
+        saplingAddr0 = self.nodes[0].z_getnewaddress('sapling')
+        saplingAddr1 = self.nodes[1].z_getnewaddress('sapling')
+
+        # Node 0 shields funds to saplingAddr0
+        recipients = [{"address": saplingAddr0, "amount": Decimal('10')}]
+        myopid = self.nodes[0].z_sendmany(taddr, recipients, 1, 0)
+        wait_and_assert_operationid_status(self.nodes[0], myopid)
+
+        self.sync_all()
+        self.nodes[2].generate(1)
+        self.sync_all()
+
+        assert_equal(self.nodes[0].z_getbalance(saplingAddr0), Decimal('10'))
+        assert_equal(self.nodes[1].z_getbalance(saplingAddr1), Decimal('0'))
+
+        # Node 1 imports saplingAddr0's key with rescan=yes, should see balance
+        sk0 = self.nodes[0].z_exportkey(saplingAddr0)
+        self.nodes[1].z_importkey(sk0, "yes")
+        assert_equal(self.nodes[1].z_getbalance(saplingAddr0), Decimal('10'))
+
+
+if __name__ == '__main__':
+    RescanImportTest().main()
---
+++ qa/rpc-tests/rescan_startup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Copyright (c) 2025 The Zcash developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
+#
+# P2: Test -rescan on startup: node restarts with -rescan, chain and wallet intact
+#
+
+import sys; assert sys.version_info < (3,), ur"This script does not run under Python 3. Please use Python 2.7.x."
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal, initialize_chain_clean, \
+    start_node, stop_node, wait_bitcoinds
+
+
+class RescanStartupTest(BitcoinTestFramework):
+
+    def setup_chain(self):
+        print("Initializing test directory "+self.options.tmpdir)
+        initialize_chain_clean(self.options.tmpdir, 1)
+
+    def setup_network(self):
+        self.nodes = []
+        self.is_network_split = False
+        self.nodes.append(start_node(0, self.options.tmpdir))
+
+    def run_test(self):
+        self.nodes[0].generate(5)
+        block_count = self.nodes[0].getblockcount()
+        assert_equal(block_count, 5)
+
+        stop_node(self.nodes[0], 0)
+        wait_bitcoinds()
+
+        # Restart with -rescan; verify chain and wallet intact
+        self.nodes[0] = start_node(0, self.options.tmpdir, ["-rescan"])
+        assert_equal(self.nodes[0].getblockcount(), 5)
+
+
+if __name__ == '__main__':
+    RescanStartupTest().main()
